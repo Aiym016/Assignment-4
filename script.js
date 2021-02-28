569dbff72376298b0bea333a4bc991024e5d4389
Aiym016 <78262569+Aiym016@users.noreply.github.com>
2021-02-28 17:50:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+alert ("Bride's qalyn calculator")
+
+
+let func=[]
+let price = 1500; //The starting bid
+
+
+function Name () {
+    var name= document.getElementById("Name").textContent; //Bride's name
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+  var num1 = Number(document.getElementById("Education").value);
+  console.log(num1);
+  var result = 1500;
+  result *= num1;
+
+function netWorth() { //Bride's family net worth
+    var selectedValue = document.getElementById("2").value;
+    switch (selectedValue) { 
+        case "2.1": price *= 2; break;
+        case "2.2": price *= 1.5; break;
+        case "2.3": price *= 1.2; break;
+    }                                                                                                           
+}
+
+function Skills () {
+    var skills= document.getElementById(skills).nodeValue;
+  var arr = [1500]  //Bride's skills
+  var result = arr.reduce(function(sum, current) {
+    return sum * current;
+  }, 0);
+  alert( result );
+
+  let skills1 = document.getElementById(skills1).checked;
+  if (skills){
+      arr.reduce(document.getElementById(skills1).value);
+  }
+  let skills2 = document.getElementById(skills2).checked;
+  if (skills){
+      arr.reduce(document.getElementById(skills2).value);
+  }
+  let skills3 = document.getElementById(skills3).checked;
+  if (skills){
+      arr.reduce(document.getElementById(skills3).value);
+  }
+  let skills4 = document.getElementById(skills4).checked;
+  if (skills){
+      arr.reduce(document.getElementById(skills4).value);
+}
+}
+
+
+
+function age () { //Bride's age
+var age= document.getElementById(age).value;
+
+let between= documemt.getElementById("Betweeen 18-23").checked;
+  if (between18) {
+      arr.reduce(document.getElementById("between18-23").value);
+  }
+
+let between24= document.getElementById("between24-27").checked;
+  if (between24) {
+      arr.reduce(document.getElementById("between24-27").value);
+  }
+
+let moreThan28 = document.getElementById("28+").checked;
+  if (morethan28) {
+      arr.reduce(document.getElementById("28+").value);
+  }
+}
+
+function reputation(a) { //Bride's reputation
+    if (a == 0.85 || a == 0.9) {
+        price *= a;
+    }
+    else {
+        price -= a;
+    }}
+
+
+
+document.getElementById("Calculate Price").onclick = function () { alert('Total price'); };
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
